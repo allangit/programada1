45467d871cb0ffaa61060f7f6e10acdc0ca425f3
alan <descartes640@gmail.com>
2023-07-15 09:48:13 -0600

    load squema for CNN

--- RedesNeuronales.py
+++ RedesNeuronales.py
@@ -2,3 +2,19 @@ import pandas as pd
 from keras.layers import Dense
 from keras.models import Sequential
 import numpy as np
+
+
+class Redes_neuronales:
+
+
+	def __init__(self):
+	
+		self.model=0
+		self.x=0
+		self.y=0
+	
+	def build_NetWork(self):
+	
+		self.model=Sequential()
+		self.model.add(Dense(12,input_dim=8,activation='relu'))
+		
